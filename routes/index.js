d587e1da2b647b3e04abba9b860f1808ff9d7ea4
xhg160 <1660480545@qq.com>
2021-01-22 16:56:16 +0800

    页面已完成

--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,20 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
-
+//注册路由
+router.get('/register', function(req, res, next) {
+  res.render('register', {  });
+});
+//文章路由
+router.get('/article', function(req, res, next) {
+  res.render('article', {  });
+});
+//查询文章路由
+router.get('/detail', function(req, res, next) {
+  res.render('detail', {  });
+});
+//写文章路由
+router.get('/write', function(req, res, next) {
+  res.render('write', {  });
+});
 module.exports = router;
